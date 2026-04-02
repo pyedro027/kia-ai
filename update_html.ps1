447c781bf6ae98f80f3f2086b46d6d3680036376
SITE.STUDY <pyedrohenryque959@gmail.com>
2026-04-01 21:41:40 -0300

    Delete update_html.ps1

--- update_html.ps1
+++
@@ -1,7 +0,0 @@
-$html = Get-Content -Path "index.html" -Raw
-
-$html = $html -replace 'src="data:image/[^;]+;base64,[^"]+"\s+alt="Logo Kiaçaí"', 'src="logo.jpg" alt="Logo Kiaçaí"'
-$html = $html -replace 'class="bottle-img"\s+src="data:image/[^;]+;base64,[^"]+"', 'class="bottle-img" src="garrafa.jpg"'
-
-[IO.File]::WriteAllText("index.html", $html)
-Write-Host "index.html updated successfully."
